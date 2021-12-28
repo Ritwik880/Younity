ef3f374a6fca86e383e236a8fc58465e096b2fc2
Ritwik880 <rittiksinha12@gmail.com>
2021-12-29 03:33:14 +0530

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,58 @@ import React from 'react'
 import logo from '../image/logo.png'
 import { Navbar, Nav, Container, Form, Modal, Button } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {faTimes} from '@fortawesome/free-solid-svg-icons'
+import {faBars} from '@fortawesome/free-solid-svg-icons'
 import '../App.css';
 const Header = () => {
+
+    const navLinks = document.getElementById('navLinks');
+    function show() {
+        navLinks.style.right= "0";
+
+
+    }
+    function hide() {
+        navLinks.style.right = "-200px";
+
+    }
+
+
+
     return (
+
+
         <>
-            <Navbar collapseOnSelect expand="lg" variant="dark" fixed="top" className='nav'>
+
+
+            <section className='header'>
+                <nav>
+                    <a href="#"><img src={logo} alt="" /></a>
+                    <div className="nav-links" id='navLinks' >
+                        {/* <FontAwesomeIcon icon={faTimes} className='fa' onClick={hide()}></FontAwesomeIcon> */}
+                        {/* <FontAwesomeIcon icon={["fas", "fa-times"]} className='fa' /> */}
+                        {/* <i class="fa fa-times" onClick={hide}></i> */}
+                        <ul>
+                            <li><a href="/">HOME</a></li>
+                            <li><a href="/about">ABOUT</a></li>
+                            <li><a href="/services">SERVICES</a></li>
+                            <li><a href="/contact">CONTACT US</a></li>
+
+                        </ul>
+                    </div>
+                    {/* <FontAwesomeIcon icon={faBars} className='fa' onClick={show()}></FontAwesomeIcon> */}
+                    {/* <i class="fa fa-bars" ></i> */}
+
+                </nav>
+                <div className="text-box">
+                    <h1>Younity Finschool</h1>
+                    <p>First Digital Student Community Of INDIA</p>
+                    <a href="#" className='button'>Get Started</a>
+                </div>
+            </section>
+            {/* <Navbar collapseOnSelect expand="lg" variant="dark" fixed="top" className='nav'>
                 <Container>
                     <Navbar.Brand href="#"><img src={logo} className='logo' /></Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -38,7 +84,8 @@ const Header = () => {
 
                     </Navbar.Collapse>
                 </Container>
-            </Navbar>
+            </Navbar> */}
+
 
         </>
     )
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,77 +1,55 @@
 import React from 'react'
-import '../App.css'
-import join from '../image/join.jpg'
+// import '../App.css'
+import image1 from '../image/image1.webp'
+import image3 from '../image/image3.jpg'
+import happy from '../image/happy.jpg'
+// import image2 from '../image/image2.heic'
 const Home = () => {
+    // const about = {
+    //     backgroundColor: '#f1f5fd',
+    //     width: '100%'
+    // }
 
     return (
 
         <>
-            <div className="header pb-6 d-flex align-items-center about">
-
-                <span className="mask about-gradient opacity-7"></span>
-                <div className="container d-flex">
-                    <div className="text-white mt-5 upper">
-                        <div className="display-2 text">
-                            Younity Finschool
-                        </div>
-                        <p className='para'>First Digital Student Community Of INDIA</p>
-                        <button className='btn btn-primary home-btn'>Get Started</button>
-
-                    </div>
-                </div>
-
-            </div>
-
-            <section name="Landing" class="landing-section container py-5 pb-0 my-4">
+            <section className="about">
+                <h1>About Younity Finschool</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
                 <div className="row">
-                    <div className="col-lg-6 col-md-12 justify-content-center align-items-center p-5 pb-0">
-                        <div class="landing-text ">
-                            <h1 class="dinot-haeding header">WHAT IS YOUNITY FINSCHOOL?
-
-                            </h1>
-                            <h5>About Younity Finschool</h5>
-
-
-
-                            <div class="d-flex pt-4 d-none d-lg-block d-xl-block d-md-block">
-                                <a href="" class="mr-2 home-btn btn btn-primary">Join Us</a>
-
-                            </div>
-                        </div>
-
-
+                    <div className="about-col">
+                        <img src={image1} alt="" />
+                        <p>Younity Finschool is a subsidiary of Younity Ed-Tech Pvt Ltd, a digital student community with more than 30,000 registered members pan-india.</p>
                     </div>
-                    <div class="col-lg-6 col-md-12 justify-content-center align-items-center p-5 pb-0 padding-none-m">
-                        <li>Younity Finschool is a subsidiary of Younity Ed-Tech Pvt Ltd, a digital student community with more than 30,000 registered members pan-india.  </li>
-                        <br></br>
-                        <li>Younity Finschool is a platform which provides with the training of certified and most renowned finance courses all across the globe.  </li>
-
-
+                    <div className="about-col">
+                        <img src={image3} alt="" />
+                        <p>Younity Finschool is a platform which provides with the training of certified and most renowned finance courses all across the globe. </p>
                     </div>
-                    <div class="arrow-right"></div>
-
                 </div>
-
             </section>
-            <section name="Landing" class="container py-5 pb-0 my-4">
+
+
+
+            <section name="Landing" class="container py-5 pb-0 my-4 advantage">
                 <div className="row">
                     <div className="col-lg-6 col-md-12 justify-content-center align-items-center p-5 pb-0">
-                    <img className='image' src={join} alt='second-img' />
+                        <img className='image' src={happy} alt='second-img' />
 
 
 
                     </div>
                     <div class="col-lg-6 col-md-12 justify-content-center align-items-center p-5 pb-0 padding-none-m">
                         <div class="landing-text ">
-                            <h1 class="dinot-haeding header">WHY JOIN YOUNITY FINSCHOL?
+                            <h3 class="heading">WHY JOIN YOUNITY FINSCHOL?
 
-                            </h1>
+                            </h3>
                             <h5>Younity Finschool Advantage</h5>
-                            <li>We provide live sessions which enables two way communication between the mentor and the student providing a better understanding of the courses.
+                            <br />
+                            <p>We provide live sessions which enables two way communication between the mentor and the student providing a better understanding of the courses.
 
-                            </li>
-                            <br></br>
-                            <li>  We also conduct doubt solving sessions and exposure to practical thinking.  </li>
+                            </p>
+
+                            <p>  We also conduct doubt solving sessions and exposure to practical thinking.  </p>
 
 
 
@@ -89,6 +67,59 @@ const Home = () => {
                 </div>
 
             </section>
+
+            <section className='cta'>
+                <h1>Vision</h1>
+                <h5>Our aim is to focus on building a group of individuals of finance enthusiasts by providing financial knowledge and training irrespective of their career path and age. Our vision is to make everyone financially independent and to make financial education accessible to people.</h5>
+            </section>
+
+            <section className="course">
+                <h1>Courses We Offer</h1>
+                <p>Launching Prep Courses for the most renowned courses in the Finance World across the globe. </p>
+              
+                <div className="row">
+                    <div className="course-col">
+                        <h2>Level 1</h2>
+                        <h3>FRM: Financial Risk Management Certification | GARP </h3>
+                        <p>Duration: 120 Hours (2 hours/chapter)</p>
+                        <p>Batches Available: </p>
+                        <li>Weekday Batch</li>
+                        <li>Weekend Batch</li>
+                        <p>Batch fee will include 2 attempts of exam, taxes, guide and institute books and financial calculator.</p>
+                    </div>
+                    <div className="course-col">
+                        <h2>Level 1</h2>
+                        <h3>CFA: Chartered Financial Analyst | USA </h3>
+                        <p>Duration: 120 Hours (2 hours/chapter)</p>
+                        <p>Batches Available: </p>
+                        <li>Weekday Batch</li>
+                        <li>Weekend Batch</li>
+                        <p>Batch fee will include 2 attempts of exam, taxes, guide and institute books and financial calculator.</p>
+                    </div>
+                    <div className="course-col">
+                        <h2>Learn Upto Level 4</h2>
+                        <h3>NCFM: NSE Academy Certification in Financial Markets | NSE India </h3>
+                        <p>Duration: 120 Hours (2 hours/chapter)</p>
+                        <p>Batches Available: </p>
+                        <li>Weekday Batch</li>
+                        <li>Weekend Batch</li>
+                        <p>Batch fee will include 2 attempts of exam, taxes, guide and institute books and financial calculator.</p>
+                    </div>
+                </div>
+            </section>
+
+            {/* <section className="footer">
+                <h4>Contact Us</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias atque, animi ducimus dolor natus consequuntur, quia expedita ipsa asperiores dolore ex
+                    <br /> veniam magnam fugit a perspiciatis esse quasi enim voluptatum!</p>
+                    <div className="icons">
+                        <i class='fa fa-facebook'></i>
+                        <i class='fa fa-twitter'></i>
+                        <i class='fa fa-instagram'></i>
+                        <i class='fa fa-linkedin'></i>
+                       
+                    </div>
+            </section> */}
 
         </>
     )
