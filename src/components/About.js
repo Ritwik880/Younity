7761192f9a14bcfaf33e3246d253e1d193fb63e8
Ritwik880 <rittiksinha12@gmail.com>
2021-12-30 07:41:15 +0530

    about page & contact page

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,52 @@
 import React from 'react'
-
+import '../App.css'
+import hero1 from '../image/hero1.jpg'
+import hero2 from '../image/hero2.jpg'
+import hero3 from '../image/hero3.jpg'
+import hero4 from '../image/hero4.jpg'
 const About = () => {
     return (
         <>
-        <div className="text-center">
-            <h1>Comming Soon..</h1>
-        </div>
-            
+            <div className="pb-6 d-flex align-items-center about-page">
+
+
+                <div className="container d-flex align-items-center">
+                    <div className="text-white mx-auto mt-5 upper">
+                        <div className="display-2 text-center">
+                            About Us
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+            <section className="container main-hero-container my-4">
+                <div className="row">
+                    <div className="col-12 col-lg-6 header-left-side d-flex justify-content-center flex-column align-items-start ">
+                        <h1 className="display-2">
+                            What Is Younity?
+                        </h1>
+                        <p className="main-hero-para">
+                            Younity Finschool is a subsidiary of Younity Ed-Tech Pvt Ltd, a digital student community with more than 30,000 registered members pan-india.
+                        </p>
+
+                    </div>
+                    {/*  --------------- main header right side--------------  */}
+                    <div className="col-12 col-lg-6 header-right-side d-flex justify-content-center align-items-center main-herosection-images">
+                        <img
+                            src={hero1}
+                            alt="heroimg"
+                            className="img-fluid"
+                        />
+                        <img
+                            src={hero2}
+                            alt="heroimg4"
+                            className="img-fluid main-hero-img2"
+                        />
+                    </div>
+                </div>
+            </section>
+
         </>
     )
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,10 +3,39 @@ import React from 'react'
 const Contact = () => {
     return (
         <>
-        <div className="text-center">
-            <h1>Comming Soon..</h1>
-        </div>
-            
+            <div className="pb-6 d-flex align-items-center contact-page">
+
+
+                <div className="container d-flex align-items-center">
+                    <div className="text-white mx-auto mt-5 upper">
+                        <div className="display-2 text-center">
+                            Contact Us
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+
+            {/* <div className="contact-section">
+                <div className="contact-info">
+                    <div><i class='fa fa-map-marker'></i>Address, City, Country</div>
+                    <div><i class='fa fa-envelope'></i>contact@gmail.com</div>
+                    <div><i class='fa fa-phone'></i>+00 000 000</div>
+                    <div><i class='fa fa-clock'></i>Mon - Fri 8:00 AM to 8:00 PM</div>
+                </div>
+                <div className="contact-form">
+                    <h2>Contact Us</h2>
+                    <form action="" className='contact'>
+                        <input type="text" name='name' class='contact-box' placeholder='Your Name*' required autoComplete='off' />
+                        <input type="number" name='name' class='contact-box' placeholder='Phone Number*' required autoComplete='off' />
+                        <input type="email" name='name' class='contact-box' placeholder='Your Email*' required autoComplete='off' />
+                        <textarea name="message" id="" cols="30" rows="10" placeholder='Message*' required></textarea>
+                        <input type="submit" name='submit' class='send-btn' value="send" />
+                    </form>
+                </div>
+            </div> */}
+
         </>
     )
 }
