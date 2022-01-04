c20678a539f93c0269242ab3d905fe40e73f656e
Ritwik880 <rittiksinha12@gmail.com>
2022-01-04 11:30:34 +0530

    added preloader

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import Home from './components/Home';
 import Header from './components/Header';
 import About from './components/About';
@@ -6,40 +7,64 @@ import Footer from './components/Footer';
 import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Courses from './components/Courses';
+import { XlviLoader } from "react-awesome-loaders";
+import { Divider } from '@material-ui/core';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000) //3 secs
+  }, [])
+
   return (
+
     <>
-      <Header />
+      {
+        loading ?
+          <div className="preLoader">
 
-      <Routes>
 
-        <Route exact path="/" element={<Home />}>
+            <XlviLoader
+              boxColors={["#EF4444", "#F59E0B", "#6366F1"]}
+              desktopSize={"50px"}
+              mobileSize={"100px"}
 
-        </Route>
+            />
+          </div>
+          :
 
-        <Route exact path="/about" element={<About />}>
+          <div>
 
-        </Route>
-        <Route exact path="/courses" element={<Courses />}>
+            <Header />
 
-        </Route>
-        
+            <Routes>
 
-        <Route exact path="/contact" element={<Contact />}>
+              <Route exact path="/" element={<Home />}>
 
-        </Route>
+              </Route>
 
-        {/* <Route exact path="/signup" element={<Signup />}>
+              <Route exact path="/about" element={<About />}>
 
-        </Route>
+              </Route>
+              <Route exact path="/courses" element={<Courses />}>
 
-        <Route exact path="/login" element={<Login />}>
+              </Route>
 
-        </Route> */}
 
-      </Routes>
-      <Footer />
+              <Route exact path="/contact" element={<Contact />}>
+
+              </Route>
+
+
+
+            </Routes>
+            <Footer />
+          </div>
+      }
 
     </>
   );
